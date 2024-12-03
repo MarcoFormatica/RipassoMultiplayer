d85d24ff9d49fea4a0fda284c5486aa215da0638
MarcoFormatica <marcoformatica@gmail.com>
2024-12-03 16:09:24 +0100

    1
    1

--- Assets/BomberMan.cs
+++ Assets/BomberMan.cs
@@ -1,8 +1,36 @@
+using Fusion;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BomberMan : MonoBehaviour
+public class BomberMan : NetworkBehaviour
 {
+    public NetworkObject normalGranade;
+    public NetworkObject specialGranade;
+    public override void Spawned()
+    {
+        base.Spawned();
+        if (HasStateAuthority)
+        {
 
+            if (PlayerConfig.playerClass == EClass.BomberMan)
+            {
+
+                GetComponentInParent<Character>().OnSpecialPowerActivate.AddListener(BomberManSpecialPower);
+                GetComponentInParent<Character>().InitializeSpecialPower(1);
+            }
+        }
+    }
+
+    public void BomberManSpecialPower()
+    {
+        ((GranateWeapon)GetComponentInParent<Character>().GetHeldWeapon()).projectilePrefab = specialGranade;
+        Invoke(nameof(BomberManRestoreNormalGranade), 5);
+    }
+    public void BomberManRestoreNormalGranade()
+    {
+        ((GranateWeapon)GetComponentInParent<Character>().GetHeldWeapon()).projectilePrefab = normalGranade;
+
+    }
 }
